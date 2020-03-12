7ff1a9b64b82459e54a58b2dac2224df5afb6d5f
Robert DiNicolas <bob@dinicolas.com>
2020-03-11 23:19:44 -0400

    Issues supports drag and drop ordering

--- src/components/issues.jsx
+++ src/components/issues.jsx
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
 import IssueCard from './issueCard';
   
 
-const Issues = ({ issues } ) => {
+const SortableItem = SortableElement(({ issue }) => <IssueCard issue={issue} />);
 
-    console.log("ISSUES", issues)
+const SortableList = SortableContainer(({ issues }) => {
     return (
-        <div className="repos-container">
-            <div className="repos-header">Order an Issue</div>
-            { issues && issues.map(issue => {
-                return (
-                    <IssueCard key={issue.id} issue={issue} />
-                )
-            })}
+        <div>
+            { issues.map((issue, index) => (
+                <SortableItem key={`issue-${index}`} index={index} issue={issue} />
+            ))}
         </div>
     );
+});
+
+class Issues extends Component {
+
+    state = { issues: [] };
+
+    componentDidMount () {
+
+        const { issues } = this.props;
+
+        this.setState({ issues });
+    }
+
+    onSortEnd = ({ oldIndex, newIndex }) => {
+
+        this.setState(({ issues }) => ({
+            issues: arrayMove(issues, oldIndex, newIndex),
+        }));
+    };
+
+    render () {
+
+        const { issues } = this.state;
+
+        return (
+            <div className="repos-container">
+                <div className="repos-header">Order an Issue</div>
+
+                { issues &&
+                    <SortableList lockAxis="y" issues={issues} onSortEnd={this.onSortEnd} />
+                }
+            </div>
+        )
+    };
 }
 
 export default Issues;
