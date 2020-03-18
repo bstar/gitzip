450f67f53c2b0213be922dad70a1ba7ad130ddbb
Robert DiNicolas <bob@dinicolas.com>
2020-03-17 21:22:51 -0400

    Fixes test where missing date results in moment creating new date each day

--- src/tests/IssueCard.test.jsx
+++ src/tests/IssueCard.test.jsx
@@ -5,13 +5,18 @@ import { createStore } from 'redux';
 import reducers from '../reducers';
 import IssueCard from '../components/issueCard';
 
+const issueFixture = {
+    title: 'test issue',
+    created_at: '2020-03-17T12:41:57Z',
+    updated_at: '2020-03-17T12:41:57Z',
+};
 
 const store = createStore(
     reducers,
 );
 
 it('renders correctly', () => {
-    const tree = renderer.create(<Provider store={store}><IssueCard /></Provider>).toJSON();
+    const tree = renderer.create(<Provider store={store}><IssueCard issue={issueFixture} /></Provider>).toJSON();
 
     expect(tree).toMatchSnapshot();
 });
